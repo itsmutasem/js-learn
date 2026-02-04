fff225d88825d6faaf14fc7690de2bd2701e2082
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-04 16:03:15 +0300

    feat(script): variables

--- 1-how-javascript-runs/script.js
+++ 1-how-javascript-runs/script.js
@@ -1,6 +1,17 @@
-console.log("Hello from script.js");
-console.warn("This is a warning");
-console.error("This is an error");
-console.log("This is a log");
-console.info("This is an info");
-console.debug("This is a debug");
+num = 10;
+flot = 3.14;
+stringvalue = "Hello world";
+booleanvalue = true;
+nullvalue = null;
+undefinedvalue = undefined;
+symbolvalue = Symbol('hello');
+bigintvalue = 1234567890123456789012345678901234567890n;
+
+console.log(num);
+console.log(flot);
+console.log(stringvalue);
+console.log(booleanvalue);
+console.log(nullvalue);
+console.log(undefinedvalue);
+console.log(symbolvalue);
+console.log(bigintvalue);
