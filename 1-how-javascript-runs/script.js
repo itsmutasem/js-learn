4f381aa8bcc7517c4a1f9afb3c0ea14b94a787e1
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-04 15:54:08 +0300

    feat(index, script): print from external script

---
+++ 1-how-javascript-runs/script.js
@@ -0,0 +1,6 @@
+console.log("Hello from script.js");
+console.warn("This is a warning");
+console.error("This is an error");
+console.log("This is a log");
+console.info("This is an info");
+console.debug("This is a debug");
