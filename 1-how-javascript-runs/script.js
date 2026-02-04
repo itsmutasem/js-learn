e28433b381d045252d28ac2c86591c04759ad239
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-04 16:24:03 +0300

    feat(script): if conditions

--- 1-how-javascript-runs/script.js
+++ 1-how-javascript-runs/script.js
@@ -1,20 +1,27 @@
-let num = 10;
-let flot = 3.14;
-let stringValue = "Hello world";
-let booleanValue = true;
-let nullValue = null;
-let undefinedValue = undefined;
-let symbolValue = Symbol('hello');
-let bigintValue = 1234567890123456789012345678901234567890n;
+// let num = 10;
+// let flot = 3.14;
+// let stringValue = "Hello world";
+// let booleanValue = true;
+// let nullValue = null;
+// let undefinedValue = undefined;
+// let symbolValue = Symbol('hello');
+// let bigintValue = 1234567890123456789012345678901234567890n;
+//
+// // can not change the value when using const
+// const username = 'John Doe';
+//
+// console.log(num);
+// console.log(flot);
+// console.log(stringValue);
+// console.log(booleanValue);
+// console.log(nullValue);
+// console.log(undefinedValue);
+// console.log(symbolValue);
+// console.log(bigintValue);
 
-// can not change the value when using const
-const username = 'John Doe';
-
-console.log(num);
-console.log(flot);
-console.log(stringValue);
-console.log(booleanValue);
-console.log(nullValue);
-console.log(undefinedValue);
-console.log(symbolValue);
-console.log(bigintValue);
+const score = "0";
+if (score === false) {
+    console.log('You passed the exam!');
+} else {
+    console.log('You failed the exam!');
+}
