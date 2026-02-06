5128e336c24844d81f47d6a5735ceca8762248dd
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-06 15:13:42 +0300

    feat(object): object inside object

--- 9-arrays-objects/object.js
+++ 9-arrays-objects/object.js
@@ -1,8 +1,11 @@
 const person = {
-    name: 'Mutasem',
+    name: { firstName: 'Mutasem', lastName: 'Mustafa' },
     age: 21,
     major: 'Backend Engineer',
     city: 'Seattle'
 }
 console.log(person.name);
+console.log(person.name.firstName);
 console.log(person);
+
+// array object
