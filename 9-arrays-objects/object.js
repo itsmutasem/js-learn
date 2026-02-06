1ddf68350ac0e2dea2193ee077c998c3b962cd14
Mutasem Mustafa <itsmutasem@gmail.com>
2026-02-06 15:35:40 +0300

    feat(object): filter

--- 9-arrays-objects/object.js
+++ 9-arrays-objects/object.js
@@ -19,3 +19,6 @@ const singleElement = personArray.find((p) => p.name === 'John');
 console.log(singleElement);
 singleElement.age = 26;
 console.log(personArray);
+// filter
+const olderThan21 = personArray.filter((p) => p.age > 21);
+console.log(olderThan21);
